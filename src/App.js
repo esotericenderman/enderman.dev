ddbbbe9dd71d3400b7c4f4a72f17db34c25ac70b
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-29 18:32:09 +0000

    Replace React with my information

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import profilePicture from "./profile/Assets/Profile Picture.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profilePicture} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Interested in <b>math</b>, <b>coding</b>, and <b>video games</b>.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
       </header>
     </div>
   );
