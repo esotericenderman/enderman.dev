5720ae559e8e498f866461dc637291aecaf5352e
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-30 16:02:29 +0000

    Update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders 'interested in' text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Interested in/);
   expect(linkElement).toBeInTheDocument();
 });
