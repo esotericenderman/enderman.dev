eef1d3f2225b8ac61da9b32cc65b36df9bfaa9ad
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-01 15:23:16 +0000

    Add script to fetch repositories

---
+++ scripts/fetchRepos.js
@@ -0,0 +1,21 @@
+// scripts/fetch-repos.js
+import { Octokit } from "@octokit/rest";
+import { mkdirSync, writeFileSync } from "fs";
+
+import privateConfig from "../src/config/privateConfig.json" with {type: "json"};
+
+const octokit = new Octokit({
+  auth: privateConfig.gitHub.token,
+});
+
+async function fetchRepositories() {
+  const { data } = await octokit.rest.repos.listForUser({
+    username: "EsotericEnderman",
+    per_page: 100,
+  });
+
+  mkdirSync("data", { recursive: true });
+  writeFileSync("data/repos.json", JSON.stringify(data, null, 2));
+}
+
+fetchRepositories().catch(console.error);
