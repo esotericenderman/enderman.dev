76334938683c2815b60afeeed98068a24d330452
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-01 15:54:02 +0000

    Add background colour

--- src/App/repositories/Repository.tsx
+++ src/App/repositories/Repository.tsx
@@ -1,6 +1,8 @@
+import "../../styles/App/repositories/Repository.css";
+
 export default function Repository(name: string) {
   return (
-    <div>
+    <div className="repository">
       <h2 className="repository-name">{name}</h2>
     </div>
   );
