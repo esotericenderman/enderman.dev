baa0880e5604be9783bc80707156c83e1be4bf96
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-01 15:57:30 +0000

    Use correct heading level

--- src/App/repositories/Repository.tsx
+++ src/App/repositories/Repository.tsx
@@ -3,7 +3,7 @@ import "../../styles/App/repositories/Repository.css";
 export default function Repository(name: string) {
   return (
     <div className="repository">
-      <h2 className="repository-name">{name}</h2>
+      <h3 className="repository-name">{name}</h3>
     </div>
   );
 }
