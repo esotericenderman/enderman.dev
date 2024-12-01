9d5ddbd5a052caf7c22ed1c9091f26b1351ec269
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-01 14:53:57 +0000

    Fix path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import profilePicture from "./profile/Assets/Profile Picture.png";
+import profilePicture from "./assets/profile/Assets/Profile Picture.png";
 import ProjectList from "./repositories/ProjectList";
 
 export default function App() {
