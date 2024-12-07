f932c1cb52a54ebe88e304a7a7049e482ea7b719
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-07 16:07:56 +0000

    Return the correct string

--- src/App/repositories/getProjectStatus.tsx
+++ src/App/repositories/getProjectStatus.tsx
@@ -1,5 +1,5 @@
 export function getProjectStatus(readMe: string): ProjectStatus | undefined {
     const regex = /(?<=!\[Project Status: )(Abandoned|Completed|Maintained)(?=\]\(.+\))/;
 
-    return readMe.match(regex)?.[0] as ProjectStatus | undefined;
+    return readMe.match(regex)?.[0]?.toLowerCase() as ProjectStatus | undefined;
 }
