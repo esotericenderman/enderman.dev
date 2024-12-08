eaabdf83d603ab94b22b1e277c5a94f246434145
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-08 13:59:28 +0000

    Sort all private repositories to the end

--- src/App/repositories/RepositoriesTable.tsx
+++ src/App/repositories/RepositoriesTable.tsx
@@ -2,5 +2,5 @@ import repos from "../../data/repos.json";
 import Repository from "./Repository";
 
 export default function RepositoriesTable() {
-  return repos.map((repo) => Repository(repo));
+  return repos.sort((a, b) => ((a.private ? 1 : 0) - (b.private ? 1 : 0))).map((repo) => Repository(repo));
 }
