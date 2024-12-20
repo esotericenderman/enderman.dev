03a6ba1d94501239ecf3cf275141b8d1d9c45193
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-20 14:35:11 +0000

    Update repository component

--- src/App/Repository/Repository.tsx
+++ src/App/Repository/Repository.tsx
@@ -1,41 +1,12 @@
-import { useEffect, useState } from "react";
-import { RestEndpointMethodTypes } from "@octokit/rest";
 import "../../styles/App/repositories/Repository.css";
-import { getProjectStatus } from "./getProjectStatus";
+import { RepositoryData } from "./RepositoryData";
 
-export default function Repository(repository: RestEndpointMethodTypes["repos"]["listForUser"]["response"]["data"][number]) {
-  const [readmeText, setReadmeText] = useState<string>("Loading README.md...");
-  console.log(readmeText);
-
-  const nameRegex = /((?<=^# ).+|(?<=<h1.+>).+(?=<\/h1>))(?=[\n\r]+)/g;
-  const descriptionRegex = new RegExp(`(?<=${nameRegex.source}\n).+(?=\n)`);
-
-  const projectStatus = getProjectStatus(repository, readmeText);
-
-  const name = (repository.private ? "[private]" : readmeText.match(nameRegex)?.[0] ?? repository.name);
-  const description = (repository.private ? "This is a private project, but may be released in the future." : readmeText.match(descriptionRegex)?.[0] ?? repository.description);
-
-  useEffect(() => {
-    async function fetchReadme() {
-      try {
-        let response = await fetch("https://raw.githubusercontent.com/" + repository.owner.login + "/" + repository.name + "/refs/heads/" + repository.default_branch + "/README.md");
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const text = await response.text();
-        setReadmeText(text);
-      } catch (error) {
-        console.error("Error fetching README.md:", error);
-      }
-    }
-    fetchReadme();
-  }, []);
-
+export default function Repository(repository: RepositoryData) {
   return (
     <div className="repository">
-      <a className="repository-name" href={"https://github.com/" + repository.full_name}><h3 className="repository-name">{repository.owner.name ?? repository.owner.login}/{name}</h3></a>
-      <p className={`repository-status repository-status-${projectStatus}`}>{projectStatus || "unknown"}</p>
-      <p className="repository-description">{description}</p>
+      <a className="repository-name" href={"https://github.com/" + repository.name}><h3 className="repository-name">{repository.name}</h3></a>
+      <p className={`repository-status repository-status-${repository.status}`}>{repository.status || "unknown"}</p>
+      <p className="repository-description">{repository.description}</p>
     </div>
   );
 }
