55a4da5059aca1ea57206428157f0ad635df07c2
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-20 14:45:13 +0000

    Account for nonexistent README files

--- src/App/Repository/RepositoryData.tsx
+++ src/App/Repository/RepositoryData.tsx
@@ -15,27 +15,31 @@ export class RepositoryData {
     }
 
     public static async fromGitHubRepository(repository: GitHubRepository, octokit: Octokit): Promise<RepositoryData | null> {
-        const response = await octokit.rest.repos.getContent({
-            owner: repository.owner.login,
-            repo: repository.name,
-            path: "README.md",
-        });
+        let readmeContent: string | undefined = undefined;
 
-        if (Array.isArray(response.data) || response.data.type !== "file") {
-            return null;
-        }
+        try {
+            const response = await octokit.rest.repos.getContent({
+                owner: repository.owner.login,
+                repo: repository.name,
+                path: "README.md",
+            });
 
-        const readmeContent = Buffer.from(response.data.content!, "base64").toString("utf8");
+            if (Array.isArray(response.data) || response.data.type !== "file") {
+                return null;
+            }
+
+            readmeContent = Buffer.from(response.data.content!, "base64").toString("utf8");
+        } catch (error) {}
 
         const nameRegex = /((?<=^# ).+|(?<=<h1.+>).+(?=<\/h1>))(?=[\n\r]+)/g;
         const descriptionRegex = new RegExp(`(?<=${nameRegex.source}\n).+(?=\n)`);
 
         const regex = /(?<=!\[Project Status: )(Abandoned|Completed|Maintained)(?=\]\(.+\))/;
 
-        const status = readmeContent.match(regex)?.[0]?.toLowerCase() as ProjectStatus | undefined;
+        const status = readmeContent?.match?.(regex)?.[0]?.toLowerCase() as ProjectStatus | undefined;
 
-        const name = (repository.private ? "[private]" : readmeContent.match(nameRegex)?.[0] ?? repository.name);
-        const description = (repository.private ? "This is a private project, but may be released in the future." : readmeContent.match(descriptionRegex)?.[0] ?? repository.description);
+        const name = repository.private ? "[private]" : readmeContent?.match(nameRegex)?.[0] ?? repository.name;
+        const description = repository.private ? "This is a private project, but may be released in the future." : readmeContent?.match(descriptionRegex)?.[0] ?? repository.description;
 
         return new RepositoryData(repository.id, name, description!, status!);
     }
