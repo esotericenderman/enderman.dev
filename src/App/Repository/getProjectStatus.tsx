c8afde4fcb18e3fc7cee941e50436a825c72aadc
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-20 14:57:01 +0000

    Delete unused files

--- src/App/Repository/getProjectStatus.tsx
+++
@@ -1,8 +0,0 @@
-import { RestEndpointMethodTypes } from "@octokit/rest";
-import { projectStatusMap } from "./projectStatusMap";
-
-export function getProjectStatus(repo: RestEndpointMethodTypes["repos"]["listForUser"]["response"]["data"][number], readMe: string): ProjectStatus | undefined {
-    const regex = /(?<=!\[Project Status: )(Abandoned|Completed|Maintained)(?=\]\(.+\))/;
-
-    return (readMe.match(regex)?.[0]?.toLowerCase() ?? projectStatusMap[repo.name]) as ProjectStatus | undefined;
-}
