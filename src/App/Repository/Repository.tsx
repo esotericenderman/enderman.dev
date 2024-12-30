46134ca3751f4d7c5c3cfd11acaa58d187f988a4
Esoteric Enderman <esotericenderman@gmail.com>
2024-12-30 23:30:37 +0000

    Make the link the whole repository

--- src/App/Repository/Repository.tsx
+++ src/App/Repository/Repository.tsx
@@ -2,10 +2,14 @@ import "../../styles/App/repositories/Repository.css";
 import { RepositoryData } from "./RepositoryData";
 
 export default function Repository(repository: RepositoryData) {
-  return (
-    <div className="repository">
-      <a className="repository-name" href={"https://github.com/" + repository.owner + "/" + repository.name}><h3 className="repository-name">{repository.displayName}</h3></a>
-      <p className={`repository-status repository-status-${repository.status}`}>{repository.status || "unknown"}</p>
-    </div>
-  );
+    return (
+        <div className="repository">
+            <a className="repository-link" href={"https://github.com/" + repository.owner + "/" + repository.name}>
+                <p className="repository-name">
+                    <h3 className="repository-name">{repository.displayName}</h3>
+                </p>
+                <p className={`repository-status repository-status-${repository.status}`}>{repository.status || "unknown"}</p>
+            </a>
+        </div>
+    );
 }
