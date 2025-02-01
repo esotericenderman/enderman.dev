c6da699e226218824c3fad112491b7eab2312562
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-01 23:38:59 +0000

    Return null if file content is not found

--- src/functions/getGitHubFileContent.ts
+++ src/functions/getGitHubFileContent.ts
@@ -2,11 +2,17 @@ import { Octokit } from "@octokit/rest";
 import { GitHubRepository } from "../types/GitHubRepository";
 
 export async function getGitHubFileContent(octokit: Octokit, repository: GitHubRepository, path: string) {
-    const response = await octokit.rest.repos.getContent({
-        owner: repository.owner.login,
-        repo: repository.name,
-        path,
-    });
+    let response;
+    
+    try {
+        response = await octokit.rest.repos.getContent({
+            owner: repository.owner.login,
+            repo: repository.name,
+            path,
+        });
+    } catch (error) {
+        return null;
+    }
 
     if (Array.isArray(response.data) || response.data.type !== "file") {
         return null;
