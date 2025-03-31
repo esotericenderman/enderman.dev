ebbc226a46b233ddebc9295d970b94f506e0d5c4
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-31 23:09:19 +0100

    Use run script instead of build

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Jekyll site
-echo "Building the Academic Pages site..."
-bundle exec jekyll build
-
-echo "Site built successfully! Check the _site directory for the compiled HTML and CSS." 
